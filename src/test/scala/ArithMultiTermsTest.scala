1afef6a5b943e68b4ff2a9cd0e8396f831fc199b
saki-aurora <skaushi1@lakeheadu.ca>
2025-03-28 15:40:05 -0400

    clean up

--- src/test/scala/ArithMultiTermsTest.scala
+++ src/test/scala/ArithMultiTermsTest.scala
@@ -1,46 +1,81 @@
-// package com.axiom
+package com.axiom
 
-// import org.scalatest._
-// import wordspec.AsyncWordSpec
-// import matchers.should.Matchers
-// import scala.scalajs.js
-// import testingutils._
-// import typings.arith.outLanguageGeneratedAstMod.{Module, BinaryExpression, NumberLiteral, Evaluation, isBinaryExpression}
+import org.scalatest._
+import wordspec._
+import matchers._
+import scala.scalajs.js
+import scala.concurrent.Future
+import scala.util.{Try, Success, Failure}
+import testingutils._
+import typings.arith.outLanguageGeneratedAstMod.{Module, Evaluation}
+import scala.scalajs.js.JSConverters._
 
-// import scala.concurrent.Future
-// import scala.util.Try
+class ArithParserMultiTermsTest extends wordspec.AsyncWordSpec with should.Matchers {
 
-// class ArithMultiTermsTest extends AnyWordSpec with Matchers {
+  override implicit def executionContext = global
 
-//   "math3" should 
-//     "work" in {
-
-//       case class TestExpr(expression: String, expectedValue: Int)
-
-//       val testExpressions = List(
-//         TestExpr("2 + 3 * 4", 14),
-//         TestExpr("(2 + 3) * 4", 20),
-//         TestExpr("5 - 2 * 3 + 1", 0),
-//         TestExpr("2 ^ 3 % 3", 2)
-//       )
-
-//       val astFuture = Try(parse(testFiles(0)).toFuture).getOrElse(Future.failed(new Exception("Parsing failed")))
-
-//       astFuture.map { module =>
-//         val result = interpretEvaluations(module).toScalaMap.map { (k, v) =>
-//           val exprStr = k.expression.$cstNode.text.trim
-//           (exprStr, v)
-//         }
-
-//         testExpressions.foreach { te =>
-//           withClue(s"Expression '${te.expression}' evaluation") {
-//             result.get(te.expression) should contain (te.expectedValue)
-//           }
-//         }
-
-//         module.name shouldBe "complexexpressions"
-//         result.size shouldBe testExpressions.size
-//       }
-//     }
   
-// }
+  def safeEvaluate(expression: String): Double = {
+    val jsExpression = expression.replaceAll("\\^", "**").replaceAll(";", "")
+    js.Dynamic.global.eval(jsExpression).asInstanceOf[Double]
+  }
+
+
+  def readFile(filePath: String): String = {
+    val fs = js.Dynamic.global.require("fs")
+    fs.readFileSync(filePath, "utf8").toString
+  }
+
+  "Test files" should {
+    "exist" in {
+      info(s"Platform: ${platform}")
+      val checkFiles = testFiles.map { fn =>
+        info(s"filename: $fn")
+        checkFileAccess(fn)
+      }.map(_ == true)
+      Future(checkFiles) map { l =>
+        l.forall(_ == true) should be (true)
+      }
+    }
+  }
+
+  "arith file" should {
+    "work for any number of terms" in {
+      
+      val filePath = testFiles(2)
+      val fileContent = readFile(filePath)
+
+      val parseFuture: Future[Module] = Try(parse(filePath).toFuture) match {
+        case Success(fut) => fut
+        case Failure(e) =>
+          info(s"Error during parsing: $e")
+          Future.failed(e)
+      }
+
+      parseFuture.flatMap { module =>
+        val results = interpretEvaluations(module).toScalaMap
+        
+        val evaluations = module.statements.filter { s =>
+          val t = s.asInstanceOf[js.Dynamic].selectDynamic("$type").asInstanceOf[String]
+          t == "Evaluation"
+        }.map(_.asInstanceOf[Evaluation])
+        results.size should be (evaluations.length)
+        module.name should be ("binaryexpressions")
+
+        Future.traverse(results.toSeq) { case (evaluation: Evaluation, computedValue: Double) =>
+          Future {
+           
+            val exprDyn = evaluation.expression.asInstanceOf[js.Dynamic]
+            val cstNode = exprDyn.selectDynamic("$cstNode")
+            val offset = cstNode.offset.asInstanceOf[Int]
+            val length = cstNode.length.asInstanceOf[Int]
+            val exprText = fileContent.substring(offset, offset + length).trim
+            val expectedValue = safeEvaluate(exprText)
+            info(s"Testing: $exprText → Computed: $computedValue, Expected: $expectedValue")
+            computedValue should equal (expectedValue)
+          }
+        }.map(_ => succeed)
+      }
+    }
+  }
+}
--- src/test/scala/ArithParserTest.scala
+++ src/test/scala/ArithParserTest.scala
@@ -3,79 +3,68 @@ package com.axiom
 import org.scalatest._
 import wordspec._
 import matchers._
-import scala.scalajs.js
-import scala.concurrent.Future
-import scala.util.{Try, Success, Failure}
-import testingutils._
-import typings.arith.outLanguageGeneratedAstMod.{Module, Evaluation}
-import scala.scalajs.js.JSConverters._
+// import scala.scalajs.js
+// import typings.std.Map as TSMap
+import testingutils.*
+import langGenAst.{Module, BinaryExpression, NumberLiteral,Evaluation,isBinaryExpression}
+// import typings.arith.outLanguageGeneratedAstMod.{Module, BinaryExpression, NumberLiteral,Evaluation,isBinaryExpression}
+
+
+
 
 class ArithParserTest extends wordspec.AsyncWordSpec with should.Matchers {
+  
+  
 
   override implicit def executionContext = global
-
-  
-  def safeEvaluate(expression: String): Double = {
-    val jsExpression = expression.replaceAll("\\^", "**").replaceAll(";", "")
-    js.Dynamic.global.eval(jsExpression).asInstanceOf[Double]
-  }
-
-
-  def readFile(filePath: String): String = {
-    val fs = js.Dynamic.global.require("fs")
-    fs.readFileSync(filePath, "utf8").toString
-  }
 
   "Test files" should {
     "exist" in {
       info(s"Platform: ${platform}")
-      val checkFiles = testFiles.map { fn =>
-        info(s"filename: $fn")
-        checkFileAccess(fn)
-      }.map(_ == true)
-      Future(checkFiles) map { l =>
-        l.forall(_ == true) should be (true)
-      }
-    }
-  }
-
-  "arith file" should {
-    "work for any number of terms" in {
       
-      val filePath = testFiles(2)
-      val fileContent = readFile(filePath)
+      // info(s"checkFiles: $checkFiles")    
+      val checkFiles =  testFiles
+          .map{fn => 
+            info(s"filename: $fn")
+            checkFileAccess(fn)}
+          .map{ _ == true }
 
-      val parseFuture: Future[Module] = Try(parse(filePath).toFuture) match {
-        case Success(fut) => fut
-        case Failure(e) =>
-          info(s"Error during parsing: $e")
-          Future.failed(e)
+      Future(checkFiles) map { l =>
+        l.forall(_ == true) should be (true) }
       }
+  }
 
-      parseFuture.flatMap { module =>
-        val results = interpretEvaluations(module).toScalaMap
-        
-        val evaluations = module.statements.filter { s =>
-          val t = s.asInstanceOf[js.Dynamic].selectDynamic("$type").asInstanceOf[String]
-          t == "Evaluation"
-        }.map(_.asInstanceOf[Evaluation])
-        results.size should be (evaluations.length)
+  "math1 file" should {
+    "work" in {
+  
+      case class TestBinaryExpression(isBinary:Boolean, left:Double, right:Double, operator:String, value:Double):
+        override def toString = s" $left $operator $right =  $value"
+
+
+      val ast = Try{parse(testFiles(0)).toFuture}.recover(e => {
+        info(s"error: $e")
+        Future.failed(e)
+      }).get
+
+      ast.map{ module =>
+        val result = interpretEvaluations(module).toScalaMap.map{(k,v) => 
+          val isBinary = isBinaryExpression(k.expression)
+          val expression = k.expression.asInstanceOf[BinaryExpression]
+          val left = expression.left.asInstanceOf[NumberLiteral].value
+          val right = expression.right.asInstanceOf[NumberLiteral].value
+          val operator = s"${expression.operator}"
+          TestBinaryExpression(isBinary, left, right, operator, v)}
+        result.foreach(r => info(s"$r"))
+        result.size should be (6)
         module.name should be ("binaryexpressions")
-
-        Future.traverse(results.toSeq) { case (evaluation: Evaluation, computedValue: Double) =>
-          Future {
-           
-            val exprDyn = evaluation.expression.asInstanceOf[js.Dynamic]
-            val cstNode = exprDyn.selectDynamic("$cstNode")
-            val offset = cstNode.offset.asInstanceOf[Int]
-            val length = cstNode.length.asInstanceOf[Int]
-            val exprText = fileContent.substring(offset, offset + length).trim
-            val expectedValue = safeEvaluate(exprText)
-            info(s"Testing: $exprText → Computed: $computedValue, Expected: $expectedValue")
-            computedValue should equal (expectedValue)
-          }
-        }.map(_ => succeed)
       }
+      
+
+      }
+
+
+
     }
   }
-}
+  
+
